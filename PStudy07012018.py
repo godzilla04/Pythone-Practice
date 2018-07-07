c2af7ad2f6cda815f609e3be0512cf07f257f25a
Godzilla04 <yoonchun@hotmail.com>
2018-07-07 16:52:43 +0900

    input 문제풀이

--- PStudy07012018.py
+++ PStudy07012018.py
@@ -1,25 +1,62 @@
-print('hello')
+# print('hello')
+#
+# #주석_한줄
+#
+# """양쪽에 따옴표는 주석"""
+#
+# #파이썬에서 스트링 입력법
+#
+# # c: 문자열 ' ' , " ", """   """", '''   ''''
+#
+# print("hello world")
+#
+# print("Hello")
+# print("World")
+#
+# print('Hello\nWorld')
+#
+#
+# print("'Hello'")
+#
+# print("\\")
+#
+# print('"C:\\Download\\hello.cpp"')
+#
+# #변수명-어떤 값을 저장하는 공간의 이름
+# #문자열() - 함수 ... 어떤 기능을 하는 코드
+# #파이썬 변수의 특성: 뭐든지 올수 없다.
+#
+# a = input("뭐든지입력해보세요")
+# print('당신의 입력한 갋은 {}입니다.'.format(a))
+#
+#
+# a=input("뭐든지입력해보세요")
+# print('당신의 입력한 갋은 {}입니다. {} 맞죠?'.format(a, a))
+#
+#
+#
+# a=123
+# b=123
+# print(a+b)
 
-#주석_한줄
 
-"""양쪽에 따옴표는 주석"""
-
-#파이썬에서 스트링 입력법
-
-# c: 문자열 ' ' , " ", """   """", '''   ''''
-
-print("hello world")
-
-print("Hello")
-print("World")
-
-print('Hello\nWorld')
+# int(), str()
 
 
-print("'Hello'")
+# #a= input('숫자를 입력해 주세요: ')
+# a= int(input('숫자를 입력해 주세요: '))
+# #b= input("숫자를 또 하나 입력해주세요: ")
+# b= int(input("숫자를 또 하나 입력해주세요: "))
+# #b=int(b)
+# print('당신이 입력한 숫자를 모두 더하면 {}입니다.' .format(a+b))
 
-print("\\")
 
-print('"C:\\Download\\hello.cpp"')
+
+
+
+
+
+
+
 
 
