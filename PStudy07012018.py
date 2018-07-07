15797a4785227d8b02b3e76815ec57d7ed94b876
Godzilla04 <yoonchun@hotmail.com>
2018-07-07 16:30:50 +0900

    print 문제 풀이

--- PStudy07012018.py
+++ PStudy07012018.py
@@ -1,3 +1,25 @@
-print("hello word")
-print("으하하하하")
-print("호호호호호호")
+print('hello')
+
+#주석_한줄
+
+"""양쪽에 따옴표는 주석"""
+
+#파이썬에서 스트링 입력법
+
+# c: 문자열 ' ' , " ", """   """", '''   ''''
+
+print("hello world")
+
+print("Hello")
+print("World")
+
+print('Hello\nWorld')
+
+
+print("'Hello'")
+
+print("\\")
+
+print('"C:\\Download\\hello.cpp"')
+
+
